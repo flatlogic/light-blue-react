19d683b70b82049411b4b6df840c4e35c5505def
DimaSavchenko <dimasauchanka@gmail.com>
2017-07-18 14:56:48 +0300

    feat(dashboard): add template, widget style, react logo

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,28 +1,30 @@
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
-// import { Link } from 'react-router-dom';
+import { Row, Col } from 'reactstrap';
 
-// import Widget from '../../components/Widget';
+import Widget from '../../components/Widget';
 import s from './Dashboard.scss';
+import logo from '../../../../flaming-octo-nemesis/src/images/react-logo.png';
+
 
 class Dashboard extends React.Component {
-  static propTypes = {
-  };
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      alert1Visible: true,
-      alert2Visible: true,
-      alert3Visible: true,
-      alert4Visible: true,
-    };
-  }
-
   render() {
     return (
-      <div>Dashboard</div>
+      <div className={s.root}>
+        <h1 className="page-title">Dashboard <small><small>The Lucky One</small></small></h1>
+        <Row>
+          <Col md={6}>
+            <Widget title={<h4>Example <span className="fw-semi-bold">Widget</span></h4>}>
+              <div>
+                <img className="pull-left mr-3" src={logo} alt="React" width="100" />
+                <p className="lead">You are looking at a completely new version of Sing App built
+                  with brand <strong>React JS</strong></p>
+                <p>Made by <a href="http://flatlogic.com" target="_blank" rel="noopener noreferrer">Flatlogic</a>.</p>
+              </div>
+            </Widget>
+          </Col>
+        </Row>
+      </div>
     );
   }
 }
