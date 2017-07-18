653b3b360c6ba3d68b24252d907223e6e9eb0740
DimaSavchenko <dimasauchanka@gmail.com>
2017-07-18 11:54:32 +0300

    feat(layout): add layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,6 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import { withRouter } from 'react-router';
+import { Switch, Route, withRouter } from 'react-router';
 
 // an example of react-router code-splitting
 /* eslint-disable */
@@ -17,26 +8,27 @@ import loadNotFound from 'bundle-loader?lazy!../../pages/notFound/NotFound';
 /* eslint-enable */
 
 import s from './Layout.scss';
-// import Header from '../Header';
-// import Footer from '../Footer';
-// import Sidebar from '../Sidebar';
+import Header from '../Header';
+import Sidebar from '../Sidebar';
 
 // Dashboard component is loaded directly as an example of server side rendering
-// import Dashboard from '../../pages/dashboard/Dashboard';
+import Dashboard from '../../pages/dashboard/Dashboard';
 
 class Layout extends React.Component {
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      sidebarOpen: false,
-    };
-  }
-
   render() {
     return (
-      <div>Layout</div>
+      <div className={s.root}>
+        <Sidebar />
+        <div className={s.wrap}>
+          <Header />
+          <main className={s.content}>
+            <Switch>
+              <Route path="/app" exact component={Dashboard} />
+            </Switch>
+          </main>
+        </div>
+      </div>
     );
   }
 }
