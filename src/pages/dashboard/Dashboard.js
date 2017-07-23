62fcb765ff56181447ab4282b84b5bacd5c016be
DimaSavchenko <dimasauchanka@gmail.com>
2017-07-23 19:47:36 +0300

    fix(dashboard): change template style

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -11,7 +11,7 @@ class Dashboard extends React.Component {
   render() {
     return (
       <div className={s.root}>
-        <h1 className="page-title">Dashboard <small><small>The Lucky One</small></small></h1>
+        <h2 className={s.pageTitle}>Dashboard <small><small>The Lucky One</small></small></h2>
         <Row>
           <Col md={6}>
             <Widget title={<h4>Example <span className="fw-semi-bold">Widget</span></h4>}>
@@ -19,7 +19,7 @@ class Dashboard extends React.Component {
                 <img className="pull-left mr-3" src={logo} alt="React" width="100" />
                 <p className="lead">You are looking at a completely new version of Sing App built
                   with brand <strong>React JS</strong></p>
-                <p>Made by <a href="http://flatlogic.com" target="_blank" rel="noopener noreferrer">Flatlogic</a>.</p>
+                <p className="fs-mini">Made by <a href="http://flatlogic.com" target="_blank" rel="noopener noreferrer">Flatlogic</a>.</p>
               </div>
             </Widget>
           </Col>
